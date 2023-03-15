12abd063716266e62f8a4aaf38cca6019be4aa2c
Dagm Kiros <114613198+dagimkiros@users.noreply.github.com>
2023-03-15 04:55:25 +0000

    added mocha-chai test

--- test/Calculator.js
+++ test/Calculator.js
@@ -3,28 +3,42 @@ const chai = require("chai");
 const expect = chai.expect;
 const asserttype = require('chai-asserttype');
 chai.use(asserttype);
-
 //load calculator code
 var Calculator = require("../Calculator");
 var calc = new Calculator();
+
 describe("Calculator Tests", function() {
+
   it("Adds 1 and 2", function() {
     expect(calc.add(1, 2)).to.be.equal(3);
   });
-  
+
+  it("Add Allows one parameter", function() {
+    expect(calc.add(1)).to.be.equal(1);
+  });
+
   it("Add Allows no parameters",function(){
-    expect(calc.add()).to.be.equal(0);
+   expect(calc.add()).to.be.equal(0);
+ });
+
+   it("Add turns strings to zeros",function(){
+   expect(calc.add("frog","toad")).to.be.number();
+ });
+
+  it("Subtract 5 from 10", function() {
+    expect(calc.subtract(10, 5)).to.be.equal(5);
   });
 
-  it("Subtract: Six - Three", function() {
-    expect(calc.subtract(6, 3)).to.be.equal(3);
+  it("Subtract allows one parameter", function() {
+    expect(calc.subtract(3)).to.be.equal(3);
   });
 
-  it("Multiply: Three times Seven", function() {
-    expect(calc.multiply(3, 7)).to.be.equal(21);
+  it("Multiply 5 x 2", function() {
+    expect(calc.multiply(5, 2)).to.be.equal(10);
   });
 
-  it("Divide: Six divided by Three", function() {
+  it("6 divided by 3", function() {
     expect(calc.divide(6, 3)).to.be.equal(2);
   });
+
 });
